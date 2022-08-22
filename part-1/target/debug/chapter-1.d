d478ef5b55901d0371eb59e9ed37b0a73b2a0fb6
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-08-22 19:05:24 +0300

    revert

---
+++ part-1/target/debug/chapter-1.d
@@ -0,0 +1,1 @@
+/home/manan/code/rust/rust-book/chapter-1/target/debug/chapter-1: /home/manan/code/rust/rust-book/chapter-1/src/main.rs
---
+++ part-1/target/debug/deps/chapter_1-0363858cb1a7b1b3.d
@@ -0,0 +1,5 @@
+/home/manan/code/rust/rust-book/chapter-1/target/debug/deps/chapter_1-0363858cb1a7b1b3: src/main.rs
+
+/home/manan/code/rust/rust-book/chapter-1/target/debug/deps/chapter_1-0363858cb1a7b1b3.d: src/main.rs
+
+src/main.rs:
